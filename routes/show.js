bacbce48eddeeb1ca49fe4fc6818909b2a5c0685
Stefanos Michaelides <stefanos2291@gmail.com>
2022-11-11 15:20:00 +0000

    Finished project and Validators, all handlers checked

--- routes/show.js
+++ routes/show.js
@@ -11,12 +11,12 @@ const { body, validationResult } = require("express-validator");
 showRouter.get("/", async (req, res) => {
   const shows = await Show.findAll();
   res.status(200).send({ shows });
-});
+}); //checked
 
 showRouter.get("/:uid", async (req, res) => {
   const show = await Show.findByPk(req.params.uid);
   res.status(200).send({ show });
-});
+}); //checked
 
 showRouter.get("/genres/:genre", async (req, res) => {
   const g = req.params.genre;
@@ -24,9 +24,15 @@ showRouter.get("/genres/:genre", async (req, res) => {
   const selectedGenre = await Show.findAll({ where: { genre: g } });
 
   res.status(200).send({ selectedGenre });
-});
+}); //checked
 
-showRouter.put("/:sid/watched", async (req, res) => {
+showRouter.put("/:sid/watched", body("rating").isInt(), async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send({ errors: errors.array() }); // server side validation isInt
+  }
+
   const theShowId = req.params.sid;
   const theShow = await Show.findByPk(theShowId);
   theShow.update(req.body);
@@ -42,7 +48,7 @@ showRouter.put(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send({ errors: errors.array() }); // server side validation
+      return res.status(400).send({ errors: errors.array() }); // server side validation nim/max
     }
 
     const theShowId = req.params.sid;
--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,26 @@
 const { Router } = require("express");
 const userRouter = Router();
 // const { Show } = require("../models/Show");
-// const { User } = require("../models/User");
+// const { User } = require("../models/User"); Do not import from their own files when relationship is established in index!
 const { User, Show } = require("../models");
 ///////////
 
 userRouter.get("/", async (req, res) => {
   const users = await User.findAll();
   res.send(users);
-});
+}); // checked
 
 userRouter.get("/:uid", async (req, res) => {
   const user = await User.findByPk(req.params.uid);
   res.status(200).send({ user }); //cannot send int back
-});
+}); // checked
 
 userRouter.get("/:uid/shows", async (req, res) => {
   const user = await User.findByPk(req.params.uid); // why null?
   console.log(user.toJSON());
   const shows = await Show.findAll({ where: { userId: user.id } });
   res.status(200).send({ shows });
-});
+}); //checked
 
 userRouter.put("/:uid/shows/:sid", async (req, res) => {
   const myUserId = req.params.uid;
@@ -34,7 +34,7 @@ userRouter.put("/:uid/shows/:sid", async (req, res) => {
   await theUser.addShow(theShow);
   console.log(req.body);
   res.status(200).send({ theUser });
-});
+}); //checked
 
 ///////////
 module.exports = userRouter;
